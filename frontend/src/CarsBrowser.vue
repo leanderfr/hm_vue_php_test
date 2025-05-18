95e72b6c5bf5b639861e7625228218e6d31edaae
leanderfr <leanderprogrammer@gmail.com>
2025-05-18 12:37:33 -0300

    Atl

--- frontend/src/CarsBrowser.vue
+++ frontend/src/CarsBrowser.vue
@@ -3,7 +3,15 @@
 
   <div class='carsBrowser'> 
 
-    <div class='carCard' v-for='car in cars' :key='car.id'>
+    <!-- car images hosted in AWS S3 -->
+    <div class='carCard' v-for='car in cars' :key='car.id' 
+        :style="{ 
+          backgroundImage: `url(https://devs-app.s3.sa-east-1.amazonaws.com/hiring_machine/car_${car.id}.png)` ,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '140px 90px',
+          backgroundPositionY: 'center'
+
+        }" >
         <span>{{car.description}}</span>
         <span>{{car.plate}}</span>
     </div>
