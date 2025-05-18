327aec0461c1d65fcbc2d9f1ff4ab46522465115
leanderfr <leanderprogrammer@gmail.com>
2025-05-18 00:24:01 -0300

    Atl

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,75 +1,89 @@
-<script >
-
-</script>
 
 <template>
-  <div class='headerBar'>
 
-    <div class='HiringMachineLogo'></div>
+  <div> <!-- this is just a fragment -->
 
-    <div class='headerText'>
-        {{ expressions.app_header }}
-    </div>
+    <div class='headerBar'>
 
-    <!-- language/country selector  -->
-    <div class="headerRight">    
+      <div class='headerLogo'></div>
 
-      <div :class="! isUSASelected ? 'flagClicked' : 'flagUnclicked' "   id='flagBRAZIL'  @click="isUSASelected = false"  >         
-        <img src="./assets/images/brazil_flag.svg" alt='' />
+      <div class='headerText' @click='isLoading=true'>
+          {{ expressions.app_header }}
       </div>
 
-      <label for="chkLanguageSelector" class="switch_language"  >
-        <input id="chkLanguageSelector" type="checkbox"  v-model="isUSASelected"    />
-        <span class="slider_language round"></span>
-      </label>
+      <!-- language/country selector  -->
+      <div class="headerRight">    
 
-      <div :class="isUSASelected ? 'flagClicked' : 'flagUnclicked' "   id='flagUSA'  @click="isUSASelected = true"  >         
-        <img src="./assets/images/usa_flag.svg" alt='' />
+        <div :class="! isUSASelected ? 'flagClicked' : 'flagUnclicked' "   id='flagBRAZIL'  @click="isUSASelected = false"  >         
+          <img src="./assets/images/brazil_flag.svg" alt='' />
+        </div>
+
+        <label for="chkLanguageSelector" class="switch_language"  >
+          <input id="chkLanguageSelector" type="checkbox"  v-model="isUSASelected"    />
+          <span class="slider_language round"></span>
+        </label>
+
+        <div :class="isUSASelected ? 'flagClicked' : 'flagUnclicked' "   id='flagUSA'  @click="isUSASelected = true"  >         
+          <img src="./assets/images/usa_flag.svg" alt='' />
+        </div>
+
       </div>
 
     </div>
 
+    <div v-show="isLoading" class='backdropTransparent'  >
+      <div id='divLoading' >&nbsp;</div>
+    </div>
   </div>
+
 </template>
 
+
+
 <script>
+// some nice effects using jquery
+import 'jquery-ui-bundle';
+import 'jquery-ui-bundle/jquery-ui.min.css';
 
-  export default {
-    data() {
-      return {
-        isUSASelected: true,
-        expressions: [],  // english/portuguese
-        isLoading: true,
-        error: null,
-        backendUrl: 'http://localhost',
-      }
-    },
+import { prepareLoadingAnimation, slidingMessage  } from './js/utils.js'
 
-    mounted() {
-      this.fetchExpressions();
-    },
 
-    methods: {
-      async fetchExpressions() {
-        this.isLoading = true;
-
-        // if language already defined and portuguese, any other situation, english
-        //let language = typeof isUSASelected != 'undefined' && ! isUSASelected  ? 'portuguese' : 'english'
-        let language = this.isUSASelected ? 'english' : 'portuguese';
-
-        fetch(`${this.backendUrl}/expressions/${language}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.expressions = data;
-          this.isLoading = false;
-        })
-        .catch((error) => console.log('erro='+error));        
-
-      },
+export default {
+  data() {
+    return {
+      isUSASelected: true,
+      expressions: [],  // english/portuguese
+      isLoading: true,
+      error: null,
+      backendUrl: 'http://localhost',  // it changes depending if it is containerized or not
     }
+  },
+
+  mounted() {
+    prepareLoadingAnimation()
+    this.fetchExpressions()
+  },
+
+  methods: {
+    async fetchExpressions() {
+      this.isLoading = true;
+
+      let language = this.isUSASelected ? 'english' : 'portuguese';
+
+      fetch(`${this.backendUrl}/expressions/${language}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.expressions = data;
+        this.isLoading = false;
+      })
+      .catch((error) => console.log('erro='+error));        
+
+    },
   }
+}
 
 </script>
+
 
 
 
---
+++ frontend/src/js/utils.js
@@ -0,0 +1,68 @@
+
+import 'spin.js/spin.css';
+import {Spinner} from 'spin.js';
+
+let alertBeep = new Audio("../assets/sounds/error_beep.mp3")
+
+const prepareLoadingAnimation = () => {
+    var opts = {
+      lines: 12 // The number of lines to draw
+    , length: 30 // The length of each line
+    , width: 18 // The line thickness
+    , radius: 42 // The radius of the inner circle
+    , scale: 0.3 // Scales overall size of the spinner
+    , corners: 3 // Corner roundness (0..1)
+    , color: 'gray' // #rgb or #rrggbb or array of colors
+    , opacity: 0.3 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 1 // Rounds per second
+    , trail: 60 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: true // Whether to render a shadow
+    , hwaccel: true // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+    ,animation: 'spinner-line-fade-quick'
+    }
+
+    // para exibir/ocultar esta div, usar as funcoes: showLoadingGif()/hideLoadingGif()
+    var divLoading = document.getElementById('divLoading');
+    new Spinner(opts).spin(divLoading);
+}
+
+/************************************************************************************************************************************************************
+check if a string is in JSON format
+************************************************************************************************************************************************************/
+const  isStringJSON = (string) => {
+    try {
+        JSON.parse(string);
+    } catch (e) {
+        return false;
+    }
+    return true;
+
+}
+
+
+
+/************************************************************************************************************************************************************
+display sliding message in red
+************************************************************************************************************************************************************/
+const slidingMessage = (html, time) => {
+
+  let slidingDIV = $('#msgRolanteErros')
+
+  slidingDIV.html('&nbsp;&nbsp;&nbsp;&nbsp;' + html);
+  slidingDIV.show("slide", { direction: "left" }, 200);
+
+  alertBeep.play()
+
+  setTimeout(function () { slidingDIV.hide("slide", { direction: "right" }, 200); }, time);
+}
+
+
+export { prepareLoadingAnimation, isStringJSON, slidingMessage };
